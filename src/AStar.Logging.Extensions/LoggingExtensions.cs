0e5662661bdb55a6805fc091ab8ee53c8c12290f
jbarden <jason.barden@outlook.com>
2024-04-22 13:23:12 +0100

    Add initial code including nuget creation and pipelines
    Code builds but has not been tested yet

---
+++ src/AStar.Logging.Extensions/LoggingExtensions.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+using Serilog;
+
+namespace AStar.Logging.Extensions;
+
+/// <summary>
+/// The <see cref="LoggingExtensions"/> class contains, as you might expect, extension methods for configuring Serilog / Application Insights.
+/// </summary>
+public static class LoggingExtensions
+{    
+    /// <summary>
+    /// The <see cref="UseSerilogLogging"/> method will add Serilog to the logging providers.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns>The original instance of <see cref="WebApplicationBuilder"/> for further method chaining.</returns>
+    public static WebApplicationBuilder UseSerilogLogging(WebApplicationBuilder builder)
+    {                
+        var services = builder.Services;
+        _ = builder.Configuration.AddJsonFile(path: "logging.json", optional: false, reloadOnChange: true);
+
+        _ = builder.Host.UseSerilog((context, loggerConfig) => loggerConfig
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {Message:lj}{NewLine}{Exception}")
+            .ReadFrom.Configuration(context.Configuration)
+            .Enrich.FromLogContext());
+
+        return builder;
+    }
+}
