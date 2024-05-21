e1e0a668f73b00bea61ebfd2dc1bd1d24586da33
Jason Barden <jason.barden@outlook.com>
2024-05-21 19:44:59 +0100

    Add the missing CreateBootstrapLogger method and
    TelemetryInitializer class

--- src/AStar.Logging.Extensions/LoggingExtensions.cs
+++ src/AStar.Logging.Extensions/LoggingExtensions.cs
@@ -9,24 +9,24 @@ namespace AStar.Logging.Extensions;
 /// </summary>
 public static class LoggingExtensions
 {
-
-/// <summary>
-/// 
-/// </summary>
-/// <param name="services"></param>
-/// <returns></returns>
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static IServiceCollection AddSerilogLogging(this IServiceCollection services)
     {
-        _=services.AddApplicationInsightsTelemetry();
+        _ = services.AddApplicationInsightsTelemetry();
         var serviceProvider = services.BuildServiceProvider();
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.ApplicationInsights(
-        serviceProvider.GetRequiredService<TelemetryConfiguration>(),
-	TelemetryConverter.Traces)
-    .CreateLogger();
+        var logger = new LoggerConfiguration()
+            .WriteTo.ApplicationInsights(serviceProvider.GetRequiredService<TelemetryConfiguration>(), TelemetryConverter.Traces)
+            .CreateLogger();
+        logger.Information("Will this log?");
+        Log.Logger = logger;
 
-    return services;
+        return services;
     }
+
     /// <summary>
     /// The <see cref="UseSerilogLogging" /> method will add Serilog to the logging providers.
     /// </summary>
@@ -45,11 +45,28 @@ Log.Logger = new LoggerConfiguration()
             _ = builder.Configuration.AddJsonFile(path: externalSettingsFile, optional: false, reloadOnChange: true);
         }
 
+        _ = builder.Configuration.AddUserSecrets<Program>();
+
         _ = builder.Host
                     .UseSerilog((context, loggerConfig) => loggerConfig
                     .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {Message:lj}{NewLine}{Exception}")
                     .ReadFrom.Configuration(context.Configuration)
                     .Enrich.FromLogContext());
+
+        return builder;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="externalSettingsFile"></param>
+    /// <returns>The original instance of the <see href="WebApplicationBuilder"></see> to facilitate method chaining (AKA fluent configuration).</returns>
+    public static WebApplicationBuilder CreateBootstrapLogger(this WebApplicationBuilder builder, string externalSettingsFile = "")
+    {
+        builder = builder.UseSerilogLogging(externalSettingsFile);
+
+        _ = builder.Services.AddSerilogLogging();
 
         return builder;
     }
